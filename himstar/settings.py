8e9fb7231941f8c68d7c732fca5ab8e7a4bea9c1
Prince Kumar <kumar.princepapneja@gmail.com>
2025-03-20 17:19:24 +0530

    migrations

--- himstar/settings.py
+++ himstar/settings.py
@@ -154,9 +154,8 @@ USE_I18N = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
-STATIC_URL = '/static/'  # URL for serving static files
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Folder to store collected static files
-
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')  # Absolute path to media folder
