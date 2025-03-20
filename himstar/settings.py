984924101ab2fe0ab670a6d5852ddf652dbda70a
Prince Kumar <kumar.princepapneja@gmail.com>
2025-03-20 17:04:36 +0530

    migrations

--- himstar/settings.py
+++ himstar/settings.py
@@ -156,7 +156,7 @@ USE_I18N = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 STATIC_URL = '/static/'
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_ROOT = '/home/ubuntu/python-himstar/staticfiles'
+STATIC_ROOT = '/python-himstar/staticfiles'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
